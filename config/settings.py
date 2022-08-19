8841087779ed695ace88ef661d07611dfe31f94d
Corentin Zheng <corentin.zheng@gmail.com>
2022-08-19 08:08:29 +0200

    🐛 allow host

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "corentinzheng.herokuapp.com"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1", "corentinzheng.herokuapp.com"]
 
 
 # Application definition
